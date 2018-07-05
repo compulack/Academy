e3af7a0517f00c1682e765e73e9bd8220ed8a765
compulack <compulack@gmail.com>
2018-07-05 17:37:36 +0200

    Update Id.js

--- academy/Id.js
+++ academy/Id.js
@@ -1,6 +1,4 @@
-var aA = function(){
-var a = document.querySelectorAll("#GET")[0].getAttribute("blgid");
-if(a == 1394216715556790088||1394216715556790080){}
+var AHAHAHAHA = document.querySelectorAll("#GET")[0].getAttribute("blgid");
+if(AHAHAHAHA == 1394216715556790088||1394216715556790080){}
 else{document.querySelectorAll("body")[0].innerHTML="";
-}
 }
