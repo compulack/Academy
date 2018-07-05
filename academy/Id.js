70d7cc6a273dfa02cf340c150f3df90de0d74be2
compulack <compulack@gmail.com>
2018-07-05 17:27:02 +0200

    Update and rename Id.txt to Id.js

---
+++ academy/Id.js
@@ -0,0 +1,6 @@
+var aA = function(){
+var a = document.querySelectorAll("#GET")[0].getAttribute("blgid");
+if(a == 1394216715556790088||1394216715556790080){}
+else{document.querySelectorAll("body")[0].innerHTML="";
+}
+}
